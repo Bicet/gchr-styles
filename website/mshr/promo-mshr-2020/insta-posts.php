729170cd57fa27e03b3724dfcca03044e05fc0fc
Bicet <bicets@gmail.com>
2020-04-09 09:53:05 +0200

    media toolkit initial commit

---
+++ website/mshr/promo-mshr-2020/insta-posts.php
@@ -0,0 +1,273 @@
+<div class="table_accordion_cell">
+    <div class="accordion accordion_gc">
+        <div class="icon_e_title_accordion">
+            <p class="title_accordion">Instagram</p>
+
+            <p class="number_universities">[nbsp]</p>
+
+            <div class="piu_accordion">
+                <div class="verticale">
+                    [nbsp]
+                </div>
+
+                <div class="orizzontale">
+                    [nbsp]
+                </div>
+            </div>
+        </div>
+
+        <div class="list_accordion">
+            <!-- ROW #1 -->
+            <div class="riga">
+                <!-- POST #1 -->
+                <div class="tweet2">
+                   <div id="cf3"><img src="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/rainbowflag-IG.jpg" alt="" width="100%"></div>
+
+					<br>
+                    <p id="ip8">The Global Campus of Human Rights is launching the second edition of its #MOOC “Monitoring the Human Rights of LGBTI Persons” 🌈<br><br>The MOOC provides an overview of human rights standards, mechanisms and practices for the protection of LGBTI persons around the world.<br>Enrol now! 👉 gchumanrights.org/mooc-lgbti<br>📷 Amsterdam Rainbow Dress, by Henri Verhoef<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights</p><br>
+                    <a onclick="copyToClipboard('#ip8')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a class="titolo_gc_europe" href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/rainbowflag-IG.jpg" target="_blank" download="">Download Pic</a>
+                </div>
+				<!-- END POST #1 -->
+				<!-- POST #2 -->
+                <div class="tweet2">
+                     <div id="cf3"><img src="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/image1.jpg" alt="" width="100%"></div>
+                    <br>
+                    <p id="ip7">In all regions of the world, Lesbian, Gay, Bisexual, Transgender and Intersex (LGBTI) persons are subject to discrimination, persecution, violence, and other forms of human rights violations. What mechanisms are there to support advocacy, legal action and social change? What can you do to break this cycle of violence?<br>Learn with us! ➡️ gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights
+</p><br><br><br>
+                     <a onclick="copyToClipboard('#ip7')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/image1.jpg" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+                </div>
+                <!-- END POST #2 -->
+            </div>
+            <!-- END ROW #1 -->
+            
+            <!-- POSTS #3 - #4 -->
+
+                <div class="riga">
+                    <div class="tweet2">
+                        <div id="cf3"><img src="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/image3.jpg" alt="" width="100%"></div>
+
+                        <br><p id="ip303">“The Universal Declaration of #HumanRights makes clear that freedom, justice and peace in the world rest on the recognition of the inherent dignity and equality of all members of the human family.” (Victor Madrigal-Borloz, @unitednationshumanrights Independent Expert on SOGI)<br><br>What mechanisms are there to support advocacy, legal action and social change? What can you do to break this cycle of violence? 👉 gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights
+						</p><br>
+                        <a class="titolo_gc_europe" style="cursor: pointer;" onclick="copyToClipboard('#ip303')">Copy Caption</a> - <a class="titolo_gc_europe" href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/image3.jpg" target="_blank" download="">Download Pic</a>
+                    </div>
+
+                    <div class="tweet2">
+                       <div class="video-container">
+							<video poster="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/cover-video1.jpg" controls>
+								<source src="https://drive.google.com/uc?export=download&id=1K1iPsXAe4-H3tJc8TBTQmihO1U5zf9xy" type='video/mp4'>
+							</video>
+						</div>
+
+                        <br><p id="ip304">In all regions of the world, social and cultural constructs of masculine and feminine prevent the full realisation of the rights of LGBTI persons. Laws, policies and practices still exist that allow discrimination, stigma, persecution, physical and psychological violence, and even death, for what is perceived as non-conformity to rigid sexual and gender norms.<br><br>Learn more: enrol in our MOOC on monitoring the human rights of LGBTI persons<br>👉 gchumanrights.org/mooc-lgbti<br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights
+</p><br>
+
+                        <a class="titolo_gc_europe" style="cursor: pointer;" onclick="copyToClipboard('#ip304')">Copy Caption</a> - <a class="titolo_gc_europe" href="https://drive.google.com/open?id=1K1iPsXAe4-H3tJc8TBTQmihO1U5zf9xy" target="_blank" download="">Download Video</a>
+                    </div>
+                </div>
+            
+           
+        </div>
+    </div>
+</div>
+    
+    
+<style type="text/css">
+.riga{
+	display:flex;
+	flex-wrap: wrap;
+	border-spacing: 20px 10px; 
+}
+
+.tweet2{
+	flex: 1 274px;
+	flex-grow: 1;
+	flex-shrink: 1;
+	flex-basis: 274px;
+	padding: 20px;
+	margin: 10px;
+	border: 1px solid #e0dfdf;
+	border-radius: 5px;
+	-moz-border-radius: 5px;
+	width: 50%;
+}
+
+@-webkit-keyframes cf3FadeInOut {
+0% {
+ opacity:1;
+}
+25% {
+opacity:1;
+}
+75% {
+opacity:0;
+}
+100% {
+opacity:0;
+}
+}
+
+@-moz-keyframes cf3FadeInOut {
+0% {
+opacity:1;
+}
+25% {
+opacity:1;
+}
+75% {
+opacity:0;
+}
+100% {
+opacity:0;
+}
+}
+
+@-o-keyframes cf3FadeInOut {
+0% {
+opacity:1;
+}
+25% {
+opacity:1;
+}
+75% {
+opacity:0;
+}
+100% {
+opacity:0;
+}
+}
+
+@keyframes cf3FadeInOut {
+0% {
+opacity:1;
+}
+25% {
+opacity:1;
+}
+75% {
+opacity:0;
+}
+100% {
+opacity:0;
+}
+}
+
+#cf3 {
+position:relative;
+height: 420px;
+max-width: 500px;
+margin:0 auto;
+}
+#cf3 img {
+position:absolute;
+left:0;
+}
+
+#cf3 img.top {
+-webkit-animation-name: cf3FadeInOut;
+-webkit-animation-timing-function: ease-in-out;
+-webkit-animation-iteration-count: infinite;
+-webkit-animation-duration: 5s;
+-webkit-animation-direction: alternate;
+
+-moz-animation-name: cf3FadeInOut;
+-moz-animation-timing-function: ease-in-out;
+-moz-animation-iteration-count: infinite;
+-moz-animation-duration: 5s;
+-moz-animation-direction: alternate;
+
+-o-animation-name: cf3FadeInOut;
+-o-animation-timing-function: ease-in-out;
+-o-animation-iteration-count: infinite;
+-o-animation-duration: 5s;
+-o-animation-direction: alternate;
+
+animation-name: cf3FadeInOut;
+animation-timing-function: ease-in-out;
+animation-iteration-count: infinite;
+animation-duration: 5s;
+animation-direction: alternate;
+}
+@media screen and (max-width: 1400px){
+#cf3 {
+    height: 350px;
+}
+}
+
+@media screen and (max-width: 1254px){
+#cf3 {
+    height: 320px;
+}
+}
+
+@media screen and (max-width: 1200px){
+#cf3 {
+    height: 340px;
+}
+}
+
+
+@media screen and (max-width: 1040px) {
+#cf3 {
+    height: 380px;
+}
+
+}
+
+
+
+
+@media screen and (max-width: 900px){
+#cf3 {
+    height: 360px;
+}
+}
+@media screen and (max-width: 815px) {
+#cf3 {
+    height:  360px;
+}
+}
+
+
+@media screen and (max-width: 768px) {
+#cf3 {
+    height: 320px;
+}
+
+    }   
+      
+.video-container {
+    position: relative;
+    padding-bottom: 100%;
+    padding-top: 0px;
+    height: 0;
+    overflow: hidden;
+}
+
+.video-container video {
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
+
+
+ <script>
+function copyToClipboard(element) {
+  var $temp = $("<textarea>");
+  $("body").append($temp);
+  var html = $(element).html();
+  html = html.replace(/<br>/g, "\n"); // or \r\n
+  html = html.replace(/&amp;/, "&");
+
+  $temp.val(html).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+$( "#FailCopy" ).click(function() {
+  alert("Well done! div #error-details has been copy to your clipboard, now paste it in the notepad or email!");
+});
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
