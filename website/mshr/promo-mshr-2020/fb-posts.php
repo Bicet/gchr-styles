29616bb3ba05ed1c7c3055983da8a76554810fce
Bicet <bicets@gmail.com>
2020-04-09 15:19:01 +0200

    Removed a couple of bugs

--- website/mshr/promo-mshr-2020/fb-posts.php
+++ website/mshr/promo-mshr-2020/fb-posts.php
@@ -46,8 +46,8 @@
                     <div class="tweet2">
 	                    <div class="video169_container"><iframe class="video169" src="https://www.youtube.com/embed/4PZB6TcyXDc" frameborder="0" allowfullscreen=""></iframe></div>
 						<br>
-                        <p id="fp1">The Global Campus of Human Rights is launching the second edition of its #MOOC “Monitoring the Human Rights of LGBTI Persons” 🌈 <br><br>The MOOC provides an overview of human rights standards, mechanisms and practices for the protection of LGBTI persons around the world.<br>Enrol now! 👉 https://gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights
-</p><br><br><br>
+                        <p id="fp1">The Global Campus of Human Rights is launching the second edition of its #MOOC “Monitoring the Human Rights of LGBTI Persons” 🌈 <br><br>The MOOC provides an overview of human rights standards, mechanisms and practices for the protection of LGBTI persons around the world.<br>Enrol now! 👉 https://gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights</p>
+                        <br><br><br>
                         <a onclick="copyToClipboard('#fp1')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/open?id=1RaQgBC7lGf0JmHunUK6Uov4x9mKjxLGA" class="titolo_gc_europe" download target="_blank">Download Video</a>
                     </div><!-- CLOSE POST #1 -->
 					
@@ -55,9 +55,9 @@
                     <div class="tweet2">
 		                <div class="video169_container"><iframe class="video169" src="https://www.youtube.com/embed/Gk4yXxBwlmw" frameborder="0" allowfullscreen=""></iframe></div>
 			            <br>
-	                    <p id="fp4">“Monitoring and reporting are at the core of our work because not only do the results shape us, but we also use them to change the system and society.” Marta Ramos, Executive Director of ILGA Portugal, will share her experience and advice in our MOOC on Monitoring the human rights of LGBTI persons.<br><br>Meet the faculty of lecturers and experts we gathered for our online course: https://gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights	                    </p>
+	                    <p id="fp2">“Monitoring and reporting are at the core of our work because not only do the results shape us, but we also use them to change the system and society.” Marta Ramos, Executive Director of ILGA Portugal, will share her experience and advice in our MOOC on Monitoring the human rights of LGBTI persons.<br><br>Meet the faculty of lecturers and experts we gathered for our online course: https://gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights</p>
 		   				<br>
-	                    <a onclick="copyToClipboard('#fp4')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/open?id=10KRGItiUrqKp2ul-ruHqQynCo9y9h29K" class="titolo_gc_europe" download target="_blank">Download Video</a>
+	                    <a onclick="copyToClipboard('#fp2')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/open?id=10KRGItiUrqKp2ul-ruHqQynCo9y9h29K" class="titolo_gc_europe" download target="_blank">Download Video</a>
                     </div>
                     <!-- CLOSE POST #2 -->					
 				</div><!-- Close ROW #1 -->
@@ -70,19 +70,20 @@
                     <div class="tweet2">
 	                     <div class="video169_container"><a href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/fb-post.jpg" class="titolo_gc_europe" download target="_blank"><img class="video169" src="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/fb-post.jpg"></a></div>
 	                    <br>
-                        <p id="fp3">Do you know the @amsterdamrainbowdress? It’s a work of art made from the flags of all the countries in the world where homosexuality is illegal.<br><br>💻 Learn more about human rights standards, mechanisms and practices for the protection of LGBTI persons around the world: https://gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights
-</p><br><br><br><br><br>
+                        <p id="fp3">Do you know the @amsterdamrainbowdress? It’s a work of art made from the flags of all the countries in the world where homosexuality is illegal.<br><br>💻 Learn more about human rights standards, mechanisms and practices for the protection of LGBTI persons around the world: https://gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights</p>
+                        <br><br><br><br><br>
                         <a onclick="copyToClipboard('#fp3')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/fb-post.jpg" class="titolo_gc_europe" download target="_blank">Download Pic</a>
                     </div><!-- CLOSE POST #3 -->
 
-					<!-- POST #2 -->
+					<!-- POST #4 -->
                     <div class="tweet2">
 	                   <div class="video169_container"><a href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/MOOC_finalframe.jpg" class="titolo_gc_europe" download target="_blank"><img class="video169" src="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/MOOC_finalframe.jpg"></a></div>
 					   <br>
-                        <p id="fp2">“Every day, millions of lesbians, gays, bisexual, trans and other gender non-conforming people are subjected to acts of great cruelty based solely on who they are or who they choose to love or desire. Denying this fact is offensive to the dignity of those targeted, as well as the global conscience." <br>Victor Madrigal-Borloz, UN Independent Expert on protection against violence and discrimination based on sexual orientation and gender identity<br>Learn how you can help in breaking this cycle of violence in our #MOOC on Monitoring the human rights of LGBTI persons: https://gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights</p><br>
-                         <a onclick="copyToClipboard('#fp2')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/MOOC_finalframe.jpg" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+                        <p id="fp4">“Every day, millions of lesbians, gays, bisexual, trans and other gender non-conforming people are subjected to acts of great cruelty based solely on who they are or who they choose to love or desire. Denying this fact is offensive to the dignity of those targeted, as well as the global conscience." <br>Victor Madrigal-Borloz, UN Independent Expert on protection against violence and discrimination based on sexual orientation and gender identity<br>Learn how you can help in breaking this cycle of violence in our #MOOC on Monitoring the human rights of LGBTI persons: https://gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights</p>
+                        <br>
+                        <a onclick="copyToClipboard('#fp4')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/MOOC_finalframe.jpg" class="titolo_gc_europe" download target="_blank">Download Pic</a>
 
-                    </div><!-- CLOSE POST #2 -->
+                    </div><!-- CLOSE POST #4 -->
 
                 </div><!-- CLOSE ROW #2 -->
             </div>
