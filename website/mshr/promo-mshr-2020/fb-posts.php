cd6b95cf5fd5d80c07bfcea912b12c7aad5af728
Bicet <bicets@gmail.com>
2020-04-09 10:54:11 +0200

    initial commit fb-posts

--- website/mshr/promo-mshr-2020/fb-posts.php
+++ website/mshr/promo-mshr-2020/fb-posts.php
@@ -1,3 +1,23 @@
+<style type="text/css">
+.riga{
+    display:flex;
+    flex-wrap: wrap;
+    border-spacing: 20px 10px; /*Optional*/
+
+}
+.tweet2{
+    flex: 1 274px;
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: 274px;
+    padding: 20px;
+    margin: 10px;
+    border: 1px solid #e0dfdf;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+}
+</style>
+
 <div class="table_accordion_cell">
         <div class="accordion accordion_gc">
             <div class="icon_e_title_accordion">
@@ -17,13 +37,12 @@
             </div>
 
             <div class="list_accordion">
-                               
-                
-                
+            
 				
 				<!-- ROW #1 -->
                 <div class="riga">
-	                <!-- POST #1 -->
+	                
+                    <!-- POST #1 -->
                     <div class="tweet2">
 	                    <div class="video169_container"><iframe class="video169" src="https://www.youtube.com/embed/4PZB6TcyXDc" frameborder="0" allowfullscreen=""></iframe></div>
 						<br>
@@ -31,18 +50,22 @@
 </p><br><br><br>
                         <a onclick="copyToClipboard('#fp1')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/open?id=1RaQgBC7lGf0JmHunUK6Uov4x9mKjxLGA" class="titolo_gc_europe" download target="_blank">Download Video</a>
                     </div><!-- CLOSE POST #1 -->
-					<!-- POST #4 -->
+					
+                    <!-- POST #2 -->
                     <div class="tweet2">
 		                <div class="video169_container"><iframe class="video169" src="https://www.youtube.com/embed/Gk4yXxBwlmw" frameborder="0" allowfullscreen=""></iframe></div>
 			            <br>
 	                    <p id="fp4">“Monitoring and reporting are at the core of our work because not only do the results shape us, but we also use them to change the system and society.” Marta Ramos, Executive Director of ILGA Portugal, will share her experience and advice in our MOOC on Monitoring the human rights of LGBTI persons.<br><br>Meet the faculty of lecturers and experts we gathered for our online course: https://gchumanrights.org/mooc-lgbti<br><br>#GCHumanRights #LoveIsLove #LGBTRightsAreHumanRights<br>@gchumanrights	                    </p>
 		   				<br>
 	                    <a onclick="copyToClipboard('#fp4')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/open?id=10KRGItiUrqKp2ul-ruHqQynCo9y9h29K" class="titolo_gc_europe" download target="_blank">Download Video</a>
-                    </div><!-- CLOSE POST #4 -->					
-					                </div><!-- Close ROW #1 -->
+                    </div>
+                    <!-- CLOSE POST #2 -->					
+				</div><!-- Close ROW #1 -->
                 
+
                 <!-- ROW #2 -->
 				<div class="riga">
+
 	                <!-- POST #3 -->
                     <div class="tweet2">
 	                     <div class="video169_container"><a href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/fb-post.jpg" class="titolo_gc_europe" download target="_blank"><img class="video169" src="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/fb-post.jpg"></a></div>
@@ -51,6 +74,7 @@
 </p><br><br><br><br><br>
                         <a onclick="copyToClipboard('#fp3')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/fb-post.jpg" class="titolo_gc_europe" download target="_blank">Download Pic</a>
                     </div><!-- CLOSE POST #3 -->
+
 					<!-- POST #2 -->
                     <div class="tweet2">
 	                   <div class="video169_container"><a href="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/MOOC_finalframe.jpg" class="titolo_gc_europe" download target="_blank"><img class="video169" src="https://gchumanrights.org/files/eiuc/mooclgbti/mediakit-2020/pics/MOOC_finalframe.jpg"></a></div>
@@ -65,35 +89,7 @@
         </div>
 </div>
 
-<style type="text/css">
-.riga
-    {
-    display:flex;
-    flex-wrap: wrap;
-    border-spacing: 20px 10px; /*Optional*/
-
-    }
- .tweet2
-    {
-
-    /*display: table-cell;*/
-    flex: 1 274px;
-    flex-grow: 1;
-    flex-shrink: 1;
-    flex-basis: 274px;
-    padding: 20px;
-    margin: 10px;
-    border: 1px solid #e0dfdf;
-    border-radius: 5px;
-    -moz-border-radius: 5px;
-
-
-    }
-
-    </style>
-
-
- <script>
+<script>
 function copyToClipboard(element) {
   var $temp = $("<textarea>");
   $("body").append($temp);
