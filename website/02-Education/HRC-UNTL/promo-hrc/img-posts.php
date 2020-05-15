70177c9abe1e769f85aa4f034bfdb9f54cb30aa3
Bicet <bicets@gmail.com>
2020-05-15 17:25:01 +0200

    code optimisations

--- website/02-Education/HRC-UNTL/promo-hrc/img-posts.php
+++ website/02-Education/HRC-UNTL/promo-hrc/img-posts.php
@@ -1,112 +1,89 @@
 <div class="table_accordion_cell">
-        <div class="accordion accordion_gc">
-            <div class="icon_e_title_accordion">
-                <p class="title_accordion">Infographics</p>
-
-                <p class="number_universities">[nbsp]</p>
-
-                <div class="piu_accordion">
-                    <div class="verticale">
-                        [nbsp]
-                    </div>
-
-                    <div class="orizzontale">
-                        [nbsp]
-                    </div>
+    <div class="accordion accordion_gc">
+        <div class="icon_e_title_accordion">
+            <p class="title_accordion">Infographics</p>
+            <p class="number_universities">[nbsp]</p>
+            <div class="piu_accordion">
+                <div class="verticale">
+                    [nbsp]
                 </div>
-            </div>
-
-            <div class="list_accordion">
-                               
-                
-               <!-- POST #1 -->
-               <div class="riga">
-					 <div class="tweet2">
-	                     <a href="files/eiuc/hrc/infographics/Infographic-1.jpg" class="titolo_gc_europe" download="Project cycle.jpg" target="_blank">
-		                   <img src="files/eiuc/hrc/infographics/Infographic-1-small.jpg" style="width:100%;" alt="placeholder">
-		                </a>
-		                <div class="pic-copyright"></div>
-               
-                       <a href="files/eiuc/hrc/infographics/Infographic-1.jpg" class="titolo_gc_europe" download="Project cycle.jpg">Download the Infographic - "Project cycle"</a>
-
-                    </div>
+                <div class="orizzontale">
+                    [nbsp]
                 </div>
-
-               <!-- POST #2 -->
-               <div class="riga">
-					 <div class="tweet2">
-	                     <a href="files/eiuc/hrc/infographics/Infographic-2.jpg" class="titolo_gc_europe" download="Studies & Training Programmes.jpg" target="_blank">
-		                   <img src="files/eiuc/hrc/infographics/Infographic-2-small.jpg" style="width:100%;" alt="placeholder">
-		                </a>
-		                <div class="pic-copyright"></div>
-               
-                       <a href="files/eiuc/hrc/infographics/Infographic-2.jpg" class="titolo_gc_europe" download="Studies & Training Programmes.jpg">Download the Infographic - "Studies & Training Programmes"</a>
-
-                    </div>
-                </div>
-
-               <!-- POST #3 -->
-               <div class="riga">
-					 <div class="tweet2">
-	                     <a href="files/eiuc/hrc/infographics/Infographic-3.jpg" class="titolo_gc_europe" download="Outreach activities.jpg" target="_blank">
-		                   <img src="files/eiuc/hrc/infographics/Infographic-3-small.jpg" style="width:100%;" alt="placeholder">
-		                </a>
-		                <div class="pic-copyright"></div>
-               
-                       <a href="files/eiuc/hrc/infographics/Infographic-3.jpg" class="titolo_gc_europe" download="Outreach activities.jpg">Download the Infographic - "Outreach activities"</a>
-
-                    </div>
-                </div>
-
-               <!-- POST #4 -->
-               <div class="riga">
-					 <div class="tweet2">
-	                     <a href="files/eiuc/hrc/infographics/Infographic-4.jpg" class="titolo_gc_europe" download="Project protagonists.jpg" target="_blank">
-		                   <img src="files/eiuc/hrc/infographics/Infographic-4-small.jpg" style="width:100%;" alt="placeholder">
-		                </a>
-		                <div class="pic-copyright"></div>
-               
-                       <a href="files/eiuc/hrc/infographics/Infographic-4.jpg" class="titolo_gc_europe" download="Project protagonists.jpg">Download the Infographic - "Project protagonists"</a>
-
-                    </div>
-                </div>
-
-
-               
-               
-
-                
             </div>
         </div>
+        <div class="list_accordion">
+            <!-- POST #1 -->
+            <div class="riga">
+                <div class="tweet2">
+                    <a href="files/eiuc/hrc/infographics/Infographic-1.jpg" class="titolo_gc_europe" download="Project cycle.jpg" target="_blank">
+                        <img src="files/eiuc/hrc/infographics/Infographic-1-small.jpg" style="width:100%;" alt="placeholder">
+                    </a>
+                    <div class="pic-copyright"></div>
+                    <a href="files/eiuc/hrc/infographics/Infographic-1.jpg" class="titolo_gc_europe" download="Project cycle.jpg">Download the Infographic - "Project cycle"</a>
+                </div>
+            </div>
+            <!-- POST #2 -->
+            <div class="riga">
+                <div class="tweet2">
+                    <a href="files/eiuc/hrc/infographics/Infographic-2.jpg" class="titolo_gc_europe" download="Studies & Training Programmes.jpg" target="_blank">
+                        <img src="files/eiuc/hrc/infographics/Infographic-2-small.jpg" style="width:100%;" alt="placeholder">
+                    </a>
+                    <div class="pic-copyright"></div>
+                    <a href="files/eiuc/hrc/infographics/Infographic-2.jpg" class="titolo_gc_europe" download="Studies & Training Programmes.jpg">Download the Infographic - "Studies & Training Programmes"</a>
+                </div>
+            </div>
+            <!-- POST #3 -->
+            <div class="riga">
+                <div class="tweet2">
+                    <a href="files/eiuc/hrc/infographics/Infographic-3.jpg" class="titolo_gc_europe" download="Outreach activities.jpg" target="_blank">
+                        <img src="files/eiuc/hrc/infographics/Infographic-3-small.jpg" style="width:100%;" alt="placeholder">
+                    </a>
+                    <div class="pic-copyright"></div>
+                    <a href="files/eiuc/hrc/infographics/Infographic-3.jpg" class="titolo_gc_europe" download="Outreach activities.jpg">Download the Infographic - "Outreach activities"</a>
+                </div>
+            </div>
+            <!-- POST #4 -->
+            <div class="riga">
+                <div class="tweet2">
+                    <a href="files/eiuc/hrc/infographics/Infographic-4.jpg" class="titolo_gc_europe" download="Project protagonists.jpg" target="_blank">
+                        <img src="files/eiuc/hrc/infographics/Infographic-4-small.jpg" style="width:100%;" alt="placeholder">
+                    </a>
+                    <div class="pic-copyright"></div>
+                    <a href="files/eiuc/hrc/infographics/Infographic-4.jpg" class="titolo_gc_europe" download="Project protagonists.jpg">Download the Infographic - "Project protagonists"</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-
 <style type="text/css">
 .riga {
-	display:flex;
-	flex-wrap: wrap;
-	border-spacing: 20px 10px; /*Optional*/
+    display: flex;
+    flex-wrap: wrap;
+    border-spacing: 20px 10px;
+    /*Optional*/
 }
 
 .tweet2 {
-	flex: 1 274px;
-	flex-grow: 1;
-	flex-shrink: 1;
-	flex-basis: 274px;
-	padding: 20px;
-	margin: 10px;
-	border: 1px solid #e0dfdf;
-	border-radius: 5px;
-	-moz-border-radius: 5px;
-	position: relative;
-	text-align: left;
-	
+    flex: 1 274px;
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: 274px;
+    padding: 20px;
+    margin: 10px;
+    border: 1px solid #e0dfdf;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    position: relative;
+    text-align: left;
+
 }
 
 .pic-copyright {
-	position: absolute;
-	bottom: 50px;
-	right: 30px;
-	font-size: 12px;
-	color: white;
+    position: absolute;
+    bottom: 50px;
+    right: 30px;
+    font-size: 12px;
+    color: white;
 }
 </style>
