acb887baef2684201bdf47044ca77fc02270af74
Bicet <bicets@gmail.com>
2020-05-26 14:46:13 +0200

    twitter initial draft

--- website/02-Education/moocs/pphr/promo-pphr-2020/tw-posts.php
+++ website/02-Education/moocs/pphr/promo-pphr-2020/tw-posts.php
@@ -19,32 +19,40 @@
             <div class="list_accordion">
                 <!--<p class="subtitle_page_programme titolo_gc_europe">Twitter</p>-->
 
+                <!-- RIGA 1 -->
                 <div class="riga">
-                    <div class="tweet2">How do we remember past atrocities and #HumanRights violations? How are memories shaped and who is involved in ‘building memory’? Enrol in the #GCHumanRights #MOOC on Memory Sites and Human Rights to find out! https://gchumanrights.org/mshr<br><br>@gchumanrights
-</p><br>
-                        <a id="tweet" class="titolo_gc_europe" href="https://twitter.com/intent/tweet?text=How%20do%20we%20remember%20past%20atrocities%20and%20%23HumanRights%20violations%3F%20How%20are%20memories%20shaped%20and%20who%20is%20involved%20in%20%E2%80%98building%20memory%E2%80%99%3F%20Enrol%20in%20the%20%23GCHumanRights%20%23MOOC%20on%20Memory%20Sites%20and%20Human%20Rights%20to%20find%20out%21%20https%3A%2F%2Fgchumanrights.org%2Fmshr%20%40gchumanrights" onclick="window.open(this.href,'','width=640,height=380,modal=yes,left=100,top=50,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no');return false">Tweet</a>
+                    <div class="tweet2">
+                        <p>
+                        Human rights are an essential part of every individual. They reflect the fact that “all human beings are born free and equal in dignity and rights”.<br>Enrolment open for the #GCHumanRights MOOC on Promoting and Protecting Human Rights: <span class="titolo_gc_europe">https://gchumanrights.org/pphr<br><br>@gchumanrights</span>
+                        </p>
+                        <br>
+                        <a id="tweet" class="titolo_gc_europe" href="https://twitter.com/intent/tweet?text=Human%20rights%20are%20an%20essential%20part%20of%20every%20individual.%20They%20reflect%20the%20fact%20that%20%E2%80%9Call%20human%20beings%20are%20born%20free%20and%20equal%20in%20dignity%20and%20rights%E2%80%9D.%20Enrolment%20open%20for%20the%20%23GCHumanRights%20MOOC%20on%20Promoting%20and%20Protecting%20Human%20Rights%3A%20https%3A%2F%2Fgchumanrights.org%2Fpphr%20%40gchumanrights" onclick="window.open(this.href,'','width=640,height=380,modal=yes,left=100,top=50,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no');return false">Tweet</a>
                     </div>
 
                     <div class="tweet2">
-                        <p>Remembrance is a way to acknowledge past suffering and to prevent it from happening again. Join renowned lecturers and experts discussing Memory Sites and Human Rights in this #GCHumanRights #MOOC: https://gchumanrights.org/mshr<br><br>@gchumanrights
-</p><br>
-                        <a id="tweet" class="titolo_gc_europe" href="https://twitter.com/intent/tweet?text=Remembrance%20is%20a%20way%20to%20acknowledge%20past%20suffering%20and%20to%20prevent%20it%20from%20happening%20again.%20Join%20renowned%20lecturers%20and%20experts%20discussing%20Memory%20Sites%20and%20Human%20Rights%20in%20this%20%23GCHumanRights%20%23MOOC%3A%20https%3A%2F%2Fgchumanrights.org%2Fmshr%20%40gchumanrights" onclick="window.open(this.href,'','width=640,height=380,modal=yes,left=100,top=50,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no');return false">Tweet</a>
+                        <p>
+                        Did you know that different regions of the world have distinctive human rights systems? The <span class="titolo_gc_europe">#GCHumanRights</span> MOOC on Promoting and Protecting Human Rights takes you to Africa, Asia, the Americas, the Arab world and Europe: <span class="titolo_gc_europe">https://gchumanrights.org/pphr<br><br>@gchumanrights</span>
+                        </p><br>
+                        <a id="tweet" class="titolo_gc_europe" href="https://twitter.com/intent/tweet?text=Did%20you%20know%20that%20different%20regions%20of%20the%20world%20have%20distinctive%20human%20rights%20systems%3F%20The%20%23GCHumanRights%20MOOC%20on%20Promoting%20and%20Protecting%20Human%20Rights%20takes%20you%20to%20Africa%2C%20Asia%2C%20the%20Americas%2C%20the%20Arab%20world%20and%20Europe%3A%20https%3A%2F%2Fgchumanrights.org%2Fpphr%20%40gchumanrights" onclick="window.open(this.href,'','width=640,height=380,modal=yes,left=100,top=50,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no');return false">Tweet</a>
                     </div>
                 </div>
 
+                <!-- RIGA 2 -->
                 <div class="riga">
                     <div class="tweet2">
-                        <p>Enrolment open for the #GCHumanRights #MOOC on Memory Sites and Human Rights! Learn about the interconnection and interdependence between history, memory, art and #HumanRights: https://gchumanrights.org/mshr<br><br>
-@gchumanrights
-
-</p><br>
-                        <a id="tweet" class="titolo_gc_europe" href="https://twitter.com/intent/tweet?text=Enrolment%20open%20for%20the%20%23GCHumanRights%20%23MOOC%20on%20Memory%20Sites%20and%20Human%20Rights%21%20Learn%20about%20the%20interconnection%20and%20interdependence%20between%20history%2C%20memory%2C%20art%20and%20%23HumanRights%3A%20https%3A%2F%2Fgchumanrights.org%2Fmshr%20%40gchumanrights" onclick="window.open(this.href,'','width=640,height=380,modal=yes,left=100,top=50,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no');return false">Tweet</a>
+                        <p>
+                        In times of violence and crisis, human rights are important safeguards. In times of peace, security and development, they are pillars sustaining our common humanity. Learn how to <span class="titolo_gc_europe">#StandUp4HumanRights</span> in the new <span class="titolo_gc_europe">#GCHumanRights</span> MOOC: <span class="titolo_gc_europe">https://gchumanrights.org/pphr<br><br>@gchumanrights
+                        </p>
+                        <br>
+                        <a id="tweet" class="titolo_gc_europe" href="https://twitter.com/intent/tweet?text=In%20times%20of%20violence%20and%20crisis%2C%20human%20rights%20are%20important%20safeguards.%20In%20times%20of%20peace%2C%20security%20and%20development%2C%20they%20are%20pillars%20sustaining%20our%20common%20humanity.%20Learn%20how%20to%20%23StandUp4HumanRights%20in%20the%20new%20%23GCHumanRights%20MOOC%3A%20https%3A%2F%2Fgchumanrights.org%2Fpphr%20%40gchumanrights" onclick="window.open(this.href,'','width=640,height=380,modal=yes,left=100,top=50,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no');return false">Tweet</a>
                     </div>
               
 
                 <div class="tweet2">
-                    <p>By remembering horrific events, memory sites help us to raise awareness of past suffering, embrace the ideal of “never again!” and inspire civic action. Discover how in the #GCHumanRights #MOOC on Memory Sites and Human Rights: https://gchumanrights.org/mshr<br><br>@gchumanrights
-</p><br>
+                    <p>
+                    Each region of the world can tell a unique story of struggles to assert Human Rights. The new <span class="titolo_gc_europe">#GCHumanRights</span> MOOC analyses current standards, mechanisms, benefits and challenges in Africa, the Americas, Arab World, Asia and Europe. <span class="titolo_gc_europe">https://gchumanrights.org/pphr<br><br>@gchumanrights</span>
+                    </p>
+                    <br>
 
                     <a id="tweet" class="titolo_gc_europe" href="https://twitter.com/intent/tweet?text=By%20remembering%20horrific%20events%2C%20memory%20sites%20help%20us%20to%20raise%20awareness%20of%20past%20suffering%2C%20embrace%20the%20ideal%20of%20%E2%80%9Cnever%20again%21%E2%80%9D%20and%20inspire%20civic%20action.%20Discover%20how%20in%20the%20%23GCHumanRights%20%23MOOC%20on%20Memory%20Sites%20and%20Human%20Rights%3A%20https%3A%2F%2Fgchumanrights.org%2Fmshr%20%40gchumanrights" onclick="window.open(this.href,'','width=640,height=380,modal=yes,left=100,top=50,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no');return false">Tweet</a>
                 </div>
@@ -54,17 +62,13 @@
 </div>
 
 <style type="text/css">
-.riga
-    {
+.riga {
     display:flex;
     flex-wrap: wrap;
     border-spacing: 20px 10px; /*Optional*/
+}
 
-    }
- .tweet2
-    {
-
-    /*display: table-cell;*/
+.tweet2{
     flex: 1 274px;
     flex-grow: 1;
     flex-shrink: 1;
@@ -74,8 +78,5 @@
     border: 1px solid #e0dfdf;
     border-radius: 5px;
     -moz-border-radius: 5px;
-
-    
-    }
-
-    </style>
+}
+ </style>
