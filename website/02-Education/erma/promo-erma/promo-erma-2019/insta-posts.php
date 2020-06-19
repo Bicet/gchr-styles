3f4de57f215cd0b7db558f786efdb8e2daccf628
Bicet <bicets@gmail.com>
2020-06-19 11:45:45 +0200

    added files for media-toolkit 2020

---
+++ website/02-Education/erma/promo-erma/promo-erma-2019/insta-posts.php
@@ -0,0 +1,348 @@
+   <div class="table_accordion_cell">
+        <div class="accordion accordion_gc">
+            <div class="icon_e_title_accordion">
+                <p class="title_accordion">Instagram</p>
+
+                <p class="number_universities">[nbsp]</p>
+
+                <div class="piu_accordion">
+                    <div class="verticale">
+                        [nbsp]
+                    </div>
+
+                    <div class="orizzontale">
+                        [nbsp]
+                    </div>
+                </div>
+            </div>
+
+            <div class="list_accordion">
+                <!-- POSTS #1 - #2 -->
+
+                <div class="riga">
+                    <div class="tweet2">
+                        <div id="cf3"><img class="bottom" src="https://drive.google.com/uc?export=view&id=1hmlG4tvriAr26fGEWK0PyoI3k0Gl6dl_" alt="" width="100%"> <img class="top" src="https://drive.google.com/uc?export=view&id=18JMB3domOz_Kdpm6EW_t3LJo0sgZnzr2" alt="" width="100%"></div>
+
+                        <p id="ip301"><br>
+                        ERMA Programme offers an intensive one-year experience in the field of democracy and human rights studies in a highly international and multicultural environment.<br>
+                        Don’t miss this unique opportunity: apply now! 👉 gchumanrights.org/erma<br><br>
+                        📆 Deadline: 13 September 2019<br>
+                        <br><br>
+                        #GCSouthEastEurope #ERMA19 #HumanRightsEducation #GCHumanRights<br>
+						@gchumanrights
+						<br></p><br><br><br><br><br>
+                        <a class="titolo_gc_europe" style="cursor: pointer;" onclick="copyToClipboard('#ip301')">Copy Caption</a> - <a class="titolo_gc_europe" href="https://drive.google.com/open?id=12anq-NKlBBnITEBf2xSu3kZUH-0wMpD5" target="_blank" download="">Download Pics</a>
+                    </div>
+
+                    <div class="tweet2">
+                        <div id="cf3"><img class="bottom" src="https://drive.google.com/uc?export=view&id=1bnvfkNN6qKWSl2j7lcsnZtw1PuOTPRvP" alt="" width="100%"> <img class="top" src="https://drive.google.com/uc?export=view&id=1ZPFNRu9mqWTrlk2iO1bI35c4_zBkOE-s" alt="" width="100%"></div>
+
+                        <p id="ip302"><br>
+						 Apply for #ERMA19 and get the chance to:<br>
+						🔹 benefit full scholarships covering tuition fees, living and travel expenses;<br>
+						🔹 study in a highly international and multicultural environment;<br>
+						🔹 acquire working and research experience throughout the 6-week internship period in countries of the SEE;<br>
+						🔹 take part in mobility period at the University of Bologna.<br><br>
+						Deadline: 13 September ➡ gchumanrights.org/erma<br><br>
+						#GCSouthEastEurope #HumanRightsEducation #GCHumanRights
+						@gchumanrights
+						<br>
+                        <br>
+                        #GCHumanRights #HumanRightsEducation<br>
+                        @gchumanrights</p><br>
+                        <a class="titolo_gc_europe" style="cursor: pointer;" onclick="copyToClipboard('#ip302')">Copy Caption</a> - <a href="https://drive.google.com/open?id=1IofjmCoVuBn1h3RJtrEbGVgCSotkR7Eq" class="titolo_gc_europe" download="" target="_blank">Download Pics</a>
+                    </div>
+                </div>
+                
+                <!-- POSTS #3 - #4 -->
+
+                <div class="riga">
+                    <div class="tweet2">
+                        <div id="cf3"><img class="bottom" src="https://drive.google.com/uc?export=view&id=1tY7t50_QcCOd0SmkQDuZB-oHih5VxO8L" alt="" width="100%"> <img class="top" src="https://drive.google.com/uc?export=view&id=1AaRRGv0OcmxCqXAL8sUTpRLWACDuQBx6" alt="" width="100%"></div>
+
+                        <p id="ip303"><br>
+                        And you, aren’t you curious? 👀<br><br>
+						Discover what the ERMA Programme has to offer: gchumanrights.org/erma<br>
+						Application deadline: 13 September<br><br>
+						#GCSouthEastEurope #ERMA19 #HumanRightsEducation #GCHumanRights<br>
+						@gchumanrights</p><br><br><br><br><br>
+                        <a class="titolo_gc_europe" style="cursor: pointer;" onclick="copyToClipboard('#ip303')">Copy Caption</a> - <a class="titolo_gc_europe" href="https://drive.google.com/open?id=1_orewWhYu1EvSV5z7cr6FxY7APNEyLof" target="_blank" download="">Download Pics</a>
+                    </div>
+
+                    <div class="tweet2">
+                        <div id="cf3"><img class="bottom" src="https://drive.google.com/uc?export=view&id=16uui_jr8-PSgP2sD0jZkO5P9B5yMO5Po" alt="" width="100%"> <img class="top" src="https://drive.google.com/uc?export=view&id=1qVFuqMB-n7XM_mBF6YyiI6MOcbbe1dwx" alt="" width="100%"></div>
+
+                        <p id="ip304"><br>
+                        Apply now for the European Regional Master’s Programme in Democracy and Human Rights in South East Europe! <br>
+						🎓🎓  Double degree (Sarajevo/Bologna) <br>
+						🌍 Internship abroad<br>
+						👍 Full scholarship (4500€ + expenses) <br><br>
+						Discover more: gchumanrights.org/erma<br>
+						Application deadline: 13 September<br><br>
+						#GCSouthEastEurope #ERMA19 #HumanRightsEducation #GCHumanRights<br>
+						@gchumanrights
+						</p><br>
+
+                        <a class="titolo_gc_europe" style="cursor: pointer;" onclick="copyToClipboard('#ip304')">Copy Caption</a> - <a class="titolo_gc_europe" href="https://drive.google.com/uc?export=view&id=1PrQ95Lvn1O6T81yk3p7L1-a0UauzMCJ2" target="_blank" download="">Download Pics</a>
+                    </div>
+                </div>
+                
+                <!-- POSTS #5 - #6 -->
+                
+                <div class="riga">
+                    <div class="tweet2">
+                        <div id="cf3"><img class="bottom" src="https://drive.google.com/uc?export=view&id=1f-7SlobSej5TRFgxmpbEQSFQNjhngu3Y" alt="" width="100%"> <img class="top" src="https://drive.google.com/uc?export=view&id=1b_frftaEV3nvPkUeFaZX-QM3FoxYqOhZ" alt="" width="100%"></div>
+
+                        <p id="ip305"><br>
+                        The European Regional Master’s Programme in Democracy and Human Rights in South East Europe is a joint initiative by the University of Sarajevo, Center for Interdisciplinary Studies (CIS) and the University of Bologna, Department of Political and Social Sciences. <br>
+						🎓 The Programme awards students with 60 ECTS and a double-degree diploma of both founding universities (UNSA and UNIBO). <br><br>
+						Applications open for #ERMA19 ➡ gchumanrights.org/erma<br>
+						📆 Deadline: 13 September<br><br>
+						#GCSouthEastEurope #HumanRightsEducation #GCHumanRights<br>
+						@gchumanrights
+						</p><br>
+                        <a class="titolo_gc_europe" style="cursor: pointer;" onclick="copyToClipboard('#ip305')">Copy Caption</a> - <a class="titolo_gc_europe" href="https://drive.google.com/open?id=1BrIqsQ4IySqwl5vGG1lKQuxYU3pen86I" target="_blank" download="">Download Pics</a>
+                    </div>
+
+                    <div class="tweet2">
+                        <div id="cf3"><img class="bottom" src="https://drive.google.com/uc?export=view&id=1tUW-vjd6lRJiJSmJG5s7_FE5bcZ_62JR" alt="" width="100%"> <img class="top" src="https://drive.google.com/uc?export=view&id=1IIIGnmURGl_uQOm5_VBd7FOhrOMrhwQl" alt="" width="100%"></div>
+
+                        <p id="ip306"><br>
+                        🌍 The European Regional Master’s Programme in Democracy and Human Rights in South East Europe (ERMA) is a point of reference for the teaching of human rights and democratisation at university level in the SEE area, and... is now accepting applications for the A.Y. 2019/2020!<br><br>
+                        📆 Deadline: 13 September 2019<br>
+                        👉 gchumanrights.org/erma<br>
+                        <br>
+                        #GCSouthEastEurope #ERMA19 #HumanRightsEducation #GCHumanRights<br>
+
+                        @gchumanrights</p><br><br><br><br>
+                        <a class="titolo_gc_europe" style="cursor: pointer;" onclick="copyToClipboard('#ip306')">Copy Caption</a> - <a class="titolo_gc_europe" href="https://drive.google.com/open?id=1eTY2Uit5hEpY8iQKEeP4gBbG8X7eKOvC" target="_blank" download="">Download Pics</a>
+                    </div>
+                </div>
+                
+                <!-- POSTS #7 - #8 -->
+                
+                <div class="riga">
+                    <div class="tweet2">
+                        <div id="cf3"><img class="bottom" src="https://drive.google.com/uc?export=view&id=1L5wQFgKJH9rrRj_QCOQulmMwmBye7s3M" alt="" width="100%"> <img class="top" src="https://drive.google.com/uc?export=view&id=1NSAvh6Hh4aMMS-LReX_1uZyCtaWmxWv4" alt="" width="100%"></div>
+
+                        <p id="ip307"><br>
+                        In its 18 years of tradition, the European Regional Master’s Programme in Democracy and Human Rights in South East Europe (ERMA) has graduated over 500 human rights professionals, who rejoined their societies as agents of change.<br><br>
+						Become a proud member of the ERMA community! 🌐 gchumanrights.org/erma<br>
+						📆 Deadline: 13 September<br><br>
+						#GCSouthEastEurope #ERMA19 #HumanRightsEducation #GCHumanRights<br>
+						@gchumanrights
+						</p><br>
+                        <a class="titolo_gc_europe" style="cursor: pointer;" onclick="copyToClipboard('#ip307')">Copy Caption</a> - <a class="titolo_gc_europe" href="https://drive.google.com/open?id=1IlkhuJj4ccrF1rWl6SssVNlakPqDWX9A" target="_blank" download="">Download Pics</a>
+                    </div>
+
+                    <div class="tweet2">
+                        <div id="cf3"><img class="bottom" src="https://drive.google.com/uc?export=view&id=1WFY6BdRjBWpZQ3VbK3o3X0L8sZh0xl4V" alt="" width="100%"> <img class="top" src="https://drive.google.com/uc?export=view&id=1QbPOjzoS1MGy1vVzH3KPYJfxEq8JbY4y" alt="" width="100%"></div>
+
+                        <p id="ip308"><br>
+                        ERMA 2018/2019 is still running, but the students already have clear ideas about it!<br><br>
+						Apply now: gchumanrights.org/erma<br>
+						📆 Deadline: 13 September<br><br>
+						#GCSouthEastEurope #ERMA19 #HumanRightsEducation #GCHumanRights<br>
+						@gchumanrights
+						</p><br><br><br><br><br>
+                        <a class="titolo_gc_europe" style="cursor: pointer;" onclick="copyToClipboard('#ip308')">Copy Caption</a> - <a class="titolo_gc_europe" href="https://drive.google.com/open?id=1T20g_P6lQff0sv1kSbVzkPbjrjg-4p7N" target="_blank" download="">Download Pics</a>
+                    </div>
+                </div>
+                
+   
+
+
+
+
+            </div>
+        </div>
+    </div><style type="text/css">
+.riga
+    {
+    display:flex;
+    flex-wrap: wrap;
+    border-spacing: 20px 10px; /*Optional*/
+
+    }
+    .tweet2
+    {
+
+    /*display: table-cell;*/
+    flex: 1 274px;
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: 274px;
+    padding: 20px;
+    margin: 10px;
+    border: 1px solid #e0dfdf;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+
+
+
+    }
+
+    @-webkit-keyframes cf3FadeInOut {
+    0% {
+     opacity:1;
+    }
+    25% {
+    opacity:1;
+    }
+    75% {
+    opacity:0;
+    }
+    100% {
+    opacity:0;
+    }
+    }
+
+    @-moz-keyframes cf3FadeInOut {
+    0% {
+    opacity:1;
+    }
+    25% {
+    opacity:1;
+    }
+    75% {
+    opacity:0;
+    }
+    100% {
+    opacity:0;
+    }
+    }
+
+    @-o-keyframes cf3FadeInOut {
+    0% {
+    opacity:1;
+    }
+    25% {
+    opacity:1;
+    }
+    75% {
+    opacity:0;
+    }
+    100% {
+    opacity:0;
+    }
+    }
+
+    @keyframes cf3FadeInOut {
+    0% {
+    opacity:1;
+    }
+    25% {
+    opacity:1;
+    }
+    75% {
+    opacity:0;
+    }
+    100% {
+    opacity:0;
+    }
+    }
+
+    #cf3 {
+    position:relative;
+    height: 420px;
+    max-width: 500px;
+    margin:0 auto;
+    }
+    #cf3 img {
+    position:absolute;
+    left:0;
+    }
+
+    #cf3 img.top {
+    -webkit-animation-name: cf3FadeInOut;
+    -webkit-animation-timing-function: ease-in-out;
+    -webkit-animation-iteration-count: infinite;
+    -webkit-animation-duration: 5s;
+    -webkit-animation-direction: alternate;
+
+    -moz-animation-name: cf3FadeInOut;
+    -moz-animation-timing-function: ease-in-out;
+    -moz-animation-iteration-count: infinite;
+    -moz-animation-duration: 5s;
+    -moz-animation-direction: alternate;
+
+    -o-animation-name: cf3FadeInOut;
+    -o-animation-timing-function: ease-in-out;
+    -o-animation-iteration-count: infinite;
+    -o-animation-duration: 5s;
+    -o-animation-direction: alternate;
+
+    animation-name: cf3FadeInOut;
+    animation-timing-function: ease-in-out;
+    animation-iteration-count: infinite;
+    animation-duration: 5s;
+    animation-direction: alternate;
+    }
+    @media screen and (max-width: 1400px){
+    #cf3 {
+        height: 350px;
+    }
+    }
+
+    @media screen and (max-width: 1254px){
+    #cf3 {
+        height: 320px;
+    }
+    }
+
+    @media screen and (max-width: 1200px){
+    #cf3 {
+        height: 340px;
+    }
+    }
+
+
+    @media screen and (max-width: 1040px) {
+    #cf3 {
+        height: 380px;
+    }
+
+    }
+
+
+
+
+    @media screen and (max-width: 900px){
+    #cf3 {
+        height: 360px;
+    }
+    }
+    @media screen and (max-width: 815px) {
+    #cf3 {
+        height:  360px;
+    }
+    }
+
+
+    @media screen and (max-width: 768px) {
+    #cf3 {
+        height: 320px;
+    }
+
+    }   
+
+    </style>
+<script>
+function copyToClipboard(element) {
+  var $temp = $("<textarea>");
+  $("body").append($temp);
+  var html = $(element).html();
+  html = html.replace(/<br>/g, "\n"); // or \r\n
+  html = html.replace(/&amp;/, "&");
+
+  $temp.val(html).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+$( "#FailCopy" ).click(function() {
+  alert("Well done! div #error-details has been copy to your clipboard, now paste it in the notepad or email!");
+});
+</script>
+
+
