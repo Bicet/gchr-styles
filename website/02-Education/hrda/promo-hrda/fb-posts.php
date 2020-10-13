0b6dd7b5f635a994bba1d241332ab74202836367
Bicet <bicets@gmail.com>
2020-10-13 12:34:23 +0200

    HRDA - Media Toolkit - Initial Commit with relative files

---
+++ website/02-Education/hrda/promo-hrda/fb-posts.php
@@ -0,0 +1,178 @@
+<div class="table_accordion_cell">
+        <div class="accordion accordion_gc">
+            <div class="icon_e_title_accordion">
+                <p class="title_accordion">Facebook</p>
+
+                <p class="number_universities">[nbsp]</p>
+
+                <div class="piu_accordion">
+                    <div class="verticale">
+                        [nbsp]
+                    </div>
+
+                    <div class="orizzontale">
+                        [nbsp]
+                    </div>
+                </div>
+            </div>
+
+            <div class="list_accordion">
+                <!--<p class="subtitle_page_programme titolo_gc_europe">Twitter</p>-->
+                
+                
+                
+				<!-- POST #1 -->
+                <div class="riga">
+                    <div class="tweet2">
+	                     <a href="https://drive.google.com/uc?export=view&id=13lBqN-O90D2ZlwTKLQAZsBd1KggQVNXF" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=13lBqN-O90D2ZlwTKLQAZsBd1KggQVNXF" width="100%" alt="placeholder">
+		                </a>
+						<br>
+                        <p id="fp1">"With ERMA you walk the talk of human rights: it is a well-thought and well-situated programme, like all its sister programmes. I truly cherished its interdisciplinary and international nature and the travels we made. My professional life, including ERMA programme, has been a series of interdisciplinary programmes choices, which have so greatly affected my worldview and approaches, summiting to my doctoral thesis."<br><br>📆 Deadline: 20 September 2020<br>🌐 Apply here: www.erma-programme.eu<br><br>@ermaprogramme
+
+	                    </p><br>
+                        <a onclick="copyToClipboard('#fp1')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=13lBqN-O90D2ZlwTKLQAZsBd1KggQVNXF" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+                    </div>
+
+                    <div class="tweet2">
+	                   <a href="https://drive.google.com/uc?export=view&id=1s8m3wTDN2_aPJii0egLqM6G3AgJGNvPS" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1s8m3wTDN2_aPJii0egLqM6G3AgJGNvPS" width="100%" alt="placeholder">
+		                </a>
+					   <br>
+                        <p id="fp2">"ERMA gave me the initial push towards work in international arena.I have felt more confident to deal with people from different backgrounds, while paying special attention to their different circumstances. Education, as the one of the fundamental rights, became my focus that I have dedicated the last 9 years to."<br><br>📆 Deadline: 20 September 2020<br>🌐 Apply here: www.erma-programme.eu<br><br>@ermaprogramme                
+	                     </p><br><br><br>
+                         <a onclick="copyToClipboard('#fp2')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=1s8m3wTDN2_aPJii0egLqM6G3AgJGNvPS" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #1 -->
+                <!-- POST #1 -->
+                <div class="riga">
+                    <div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=1avUY8Bg54iJ_X-t1BngNHnfeiYNPPeK6" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1avUY8Bg54iJ_X-t1BngNHnfeiYNPPeK6" width="100%" alt="placeholder">
+		                </a>
+
+	                    <br>
+                        <p id="fp3">"Do not hesitate: ERMA is the best choice if you trust that human rights are important."<br><br>📆 Deadline: 20 September 2020<br>🌐 Apply here: www.erma-programme.eu<br><br>@ermaprogramme
+	                    </p><br><br><br>
+                        <a onclick="copyToClipboard('#fp3')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=1avUY8Bg54iJ_X-t1BngNHnfeiYNPPeK6" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+                    </div>
+
+                    <div class="tweet2">
+	                <a href="https://drive.google.com/uc?export=view&id=1rTRCW7umAqyrlMcIqr9gmOmZHj_-nGYM" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1rTRCW7umAqyrlMcIqr9gmOmZHj_-nGYM" width="100%" alt="placeholder">
+		                </a>
+					   <br>
+                        <p id="fp4">"ERMA is a unique programme that allows students to acquire not only academic knowledge, but also to meet amazing people that are united in the idea to make this world a better place for everyone."<br><br>📆 Deadline: 20 September 2020<br>🌐 Apply here: www.erma-programme.eu<br><br>@ermaprogramme
+                 
+	                     </p><br>
+                         <a onclick="copyToClipboard('#fp4')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=1rTRCW7umAqyrlMcIqr9gmOmZHj_-nGYM" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #1 -->
+				<!-- POST #1 -->
+                <div class="riga">
+                    <div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=1Bg5X2RWWY8Ur6OBh3lU8eFyo0m0yYsV2" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1Bg5X2RWWY8Ur6OBh3lU8eFyo0m0yYsV2" width="100%" alt="placeholder">
+		                </a>
+
+	                    <br>
+                        <p id="fp5">"ERMA was a chance to get the most prominent lecturers and experts from Eastern Europe, and wider, and it was a great learning experience. ERMA is excellent opportunity for networking as one will become friends with students from the region which is excellent social capital for future work. This aspect was quite beneficial for me and my work as a development practitioner in the Western Balkans region."<br><br>📆 Deadline: 20 September 2020<br>🌐 Apply here: www.erma-programme.eu<br><br>@ermaprogramme
+	                        	                
+                        </p><br>
+                        <a onclick="copyToClipboard('#fp5')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=1Bg5X2RWWY8Ur6OBh3lU8eFyo0m0yYsV2" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+                    </div>
+
+                    <div class="tweet2">
+	                <a href="https://drive.google.com/uc?export=view&id=1iA5Tdsb5HwTNsYCm6JFepeDUzpimKRmv" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1iA5Tdsb5HwTNsYCm6JFepeDUzpimKRmv" width="100%" alt="placeholder">
+		                </a>
+					   <br>
+                        <p id="fp6">"The regional character of the ERMA Programme not only offered a variety of views but also provided me with an opportunity to meet some of the most interesting and promising young leaders in the region. Attending this truly unique Programme was one of the best professional and personal decisions I ever made."<br><br>📆 Deadline: 20 September 2020<br>🌐 Apply here: www.erma-programme.eu<br><br>@ermaprogramme              
+	                     </p><br><br><br>
+                         <a onclick="copyToClipboard('#fp6')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=1iA5Tdsb5HwTNsYCm6JFepeDUzpimKRmv" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #1 -->
+				<div class="riga">
+                    <div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=1Wc3_64AF0FATMUMhUvz65-pFj_ptPOCu" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1Wc3_64AF0FATMUMhUvz65-pFj_ptPOCu" width="100%" alt="placeholder">
+		                </a>
+
+	                    <br>
+                        <p id="fp5">"ERMA is like a hurricane: it brings headstrong Balkan people together, swiftly slams their small universes, mercilessly burns their history books, cynically curses their governments, and finally sends them back – ready to fight for a better home."<br><br>📆 Deadline: 20 September 2020<br>🌐 Apply here: www.erma-programme.eu<br><br>@ermaprogramme
+	                        	                
+                        </p><br><br><br><br><br>
+                        <a onclick="copyToClipboard('#fp5')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=1Wc3_64AF0FATMUMhUvz65-pFj_ptPOCu" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+                    </div>
+
+                    <div class="tweet2">
+	                <a href="https://drive.google.com/uc?export=view&id=1q5se6jvG5oYoqDfLuE337nh0wOEWeSZ9" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1q5se6jvG5oYoqDfLuE337nh0wOEWeSZ9" width="100%" alt="placeholder">
+		                </a>
+					   <br>
+                        <p id="fp6">"There are only a very few places in Europe where studying human rights and democratization can be as exciting as it is in Sarajevo. Once you step out of the classroom, you literally can see what human rights protection and democratization – or the lack of them- is all about. ERMA offers a unique combination of rigorous academic training and an environment which confronts you on a daily basis with a reality check of what you studied."<br><br>📆 Deadline: 20 September 2020<br>🌐 Apply here: www.erma-programme.eu<br><br>@ermaprogramme              
+	                     </p><br>
+                         <a onclick="copyToClipboard('#fp6')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=1q5se6jvG5oYoqDfLuE337nh0wOEWeSZ9" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #1 -->
+
+
+                
+                                
+                
+                
+
+            </div>
+        </div>
+</div>
+
+<style type="text/css">
+.riga
+    {
+    display:flex;
+    flex-wrap: wrap;
+    border-spacing: 20px 10px; /*Optional*/
+
+    }
+ .tweet2
+    {
+
+    /*display: table-cell;*/
+    flex: 1 274px;
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: 274px;
+    padding: 20px;
+    margin: 10px;
+    border: 1px solid #e0dfdf;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+
+
+    }
+
+    </style>
+
+
+ <script>
+function copyToClipboard(element) {
+  var $temp = $("<textarea>");
+  $("body").append($temp);
+  var html = $(element).html();
+  html = html.replace(/<br>/g, "\n"); // or \r\n
+  html = html.replace(/&amp;/, "&");
+
+  $temp.val(html).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+$( "#FailCopy" ).click(function() {
+  alert("Well done! div #error-details has been copy to your clipboard, now paste it in the notepad or email!");
+});
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
---
+++ website/02-Education/hrda/promo-hrda/img-posts.php
@@ -0,0 +1,126 @@
+<div class="table_accordion_cell">
+        <div class="accordion accordion_gc">
+            <div class="icon_e_title_accordion">
+                <p class="title_accordion">Pictures</p>
+
+                <p class="number_universities">[nbsp]</p>
+
+                <div class="piu_accordion">
+                    <div class="verticale">
+                        [nbsp]
+                    </div>
+
+                    <div class="orizzontale">
+                        [nbsp]
+                    </div>
+                </div>
+            </div>
+
+            <div class="list_accordion">
+                               
+                
+                 <!-- POST #1 -->
+                <div class="riga">
+					 <div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=18rHCoRc9Upyok0YmazPjPqr3RGWjnf0V" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=18rHCoRc9Upyok0YmazPjPqr3RGWjnf0V" width="100%" alt="placeholder">
+		                </a>
+               
+                       <a href="https://drive.google.com/uc?export=view&id=18rHCoRc9Upyok0YmazPjPqr3RGWjnf0V" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #1 -->
+
+                                
+                <!-- POST #2 -->
+                <div class="riga">
+	            	<div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=1aaC45FFEsFzf5v07LZT8jWpqgdkf4SCA" alt="image_description" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1aaC45FFEsFzf5v07LZT8jWpqgdkf4SCA" alt="image_description" width="100%" alt="placeholder">
+		                </a>
+               
+                       <a href="https://drive.google.com/uc?export=view&id=1aaC45FFEsFzf5v07LZT8jWpqgdkf4SCA" alt="image_description" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+                    </div>
+                </div><!-- Close POST #2 -->
+                
+                <!-- POST #3 -->
+                <div class="riga">                   
+					<div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=1dlYOc3PTDR0l775IxjXmvx-7HEceSX69" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1dlYOc3PTDR0l775IxjXmvx-7HEceSX69" width="100%" alt="placeholder">
+		                </a>
+               
+                       <a href="https://drive.google.com/uc?export=view&id=1dlYOc3PTDR0l775IxjXmvx-7HEceSX69" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #3 -->
+				
+				
+				<!-- POST #4 -->
+                <div class="riga">                   
+					<div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=1uE2HWzUEszbGiI3ecqy2Ceg1QqDMVInV" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1uE2HWzUEszbGiI3ecqy2Ceg1QqDMVInV" width="100%" alt="placeholder">
+		                </a>
+               
+                       <a href="https://drive.google.com/uc?export=view&id=1uE2HWzUEszbGiI3ecqy2Ceg1QqDMVInV" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #4 -->
+                
+                <!-- POST #5 -->
+                <div class="riga">                   
+					<div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=1-xzTC5Y2QnRHLWKgJqvBzXEt-3mJXEYU" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1-xzTC5Y2QnRHLWKgJqvBzXEt-3mJXEYU" width="100%" alt="placeholder">
+		                </a>
+               
+                       <a href="https://drive.google.com/uc?export=view&id=1-xzTC5Y2QnRHLWKgJqvBzXEt-3mJXEYU" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #5 -->
+				<!-- POST #6 -->
+                <div class="riga">                   
+					<div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=1aJDhuH8yqu0FEtX0wCSB8kQXqacOwbLP" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1aJDhuH8yqu0FEtX0wCSB8kQXqacOwbLP" width="100%" alt="placeholder">
+		                </a>
+               
+                       <a href="https://drive.google.com/uc?export=view&id=1aJDhuH8yqu0FEtX0wCSB8kQXqacOwbLP" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #6 -->
+                           
+
+            </div>
+        </div>
+</div>
+
+<style type="text/css">
+.riga
+    {
+    display:flex;
+    flex-wrap: wrap;
+    border-spacing: 20px 10px; /*Optional*/
+
+    }
+ .tweet2
+    {
+
+    /*display: table-cell;*/
+    flex: 1 274px;
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: 274px;
+    padding: 20px;
+    margin: 10px;
+    border: 1px solid #e0dfdf;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+
+
+    }
+
+    </style>
+
+
