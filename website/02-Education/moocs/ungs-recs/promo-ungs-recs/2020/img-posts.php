701622afd568d2b385ad34ac02d138211b87e7e3
Bicet <bicets@gmail.com>
2020-11-05 15:59:43 +0100

    MOOC UNGS RECS: Various
    - Course Outline Update
    - Media-Toolkit Update
    - Media-Toolkit Images Update

--- website/02-Education/moocs/ungs-recs/promo-ungs-recs/2020/img-posts.php
+++ website/02-Education/moocs/ungs-recs/promo-ungs-recs/2020/img-posts.php
@@ -1,3 +1,13 @@
+<!-- ORIGINAL LINKS 
+
+files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic01.jpg
+files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic01-small.jpg
+files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic02.jpg
+files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic03.jpg
+files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic04.jpg
+
+-->
+
 <div class="table_accordion_cell">
         <div class="accordion accordion_gc">
             <div class="icon_e_title_accordion">
@@ -22,12 +32,12 @@
                  <!-- POST #1 -->
                <div class="riga">
 					 <div class="tweet2">
-	                     <a href="files/eiuc/mooc-ungs/media-toolkit-pictures/pic1.jpg" class="titolo_gc_europe" download="Photo © UNICEF/UNI43560/LeMoyne.jpg" target="_blank">
-		                   <img src="files/eiuc/mooc-ungs/media-toolkit-pictures/pic1_small.jpg" style="width:100%;" alt="placeholder">
+	                     <a href="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic01.jpg" class="titolo_gc_europe" download="Photo © Richard Ross, juvenile-in-justice.com.jpg" target="_blank">
+		                   <img src="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic01-small.jpg" style="width:100%;" alt="placeholder">
 		                </a>
-		                <div class="pic-copyright">Photo © UNICEF/UNI43560/LeMoyne</div>
+		                <div class="pic-copyright">Photo © Richard Ross, juvenile-in-justice.com.jpg</div>
                
-                       <a href="files/eiuc/mooc-ungs/media-toolkit-pictures/pic1.jpg" class="titolo_gc_europe" download="Photo © UNICEF/UNI43560/LeMoyne.jpg" target="_blank">Download Pic</a>
+                       <a href="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic01.jpg" class="titolo_gc_europe" download="Photo © Richard Ross, juvenile-in-justice.com.jpg" target="_blank">Download Pic</a>
 
                     </div>
                 </div><!-- Close POST #1 -->
@@ -36,12 +46,12 @@
                 <!-- POST #2 -->
                 <div class="riga">
 					 <div class="tweet2">
-	                     <a href="files/eiuc/mooc-ungs/media-toolkit-pictures/pic2.jpg" class="titolo_gc_europe" download="Indonesia - Central JavaJuvenile prison Kutoarjo.jpg" target="_blank">
-		                   <img src="files/eiuc/mooc-ungs/media-toolkit-pictures/pic2.jpg" style="width:100%;" alt="placeholder">
+	                     <a href="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic02.jpg" class="titolo_gc_europe" download="MOOC UNGS-RECS - Pic02.jpg" target="_blank">
+		                   <img src="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic02.jpg" style="width:100%;" alt="placeholder">
 		                </a>
-		                <div class="pic-copyright">Indonesia - Juvenile prison</div>
+		                <div class="pic-copyright"></div>
                
-                       <a href="files/eiuc/mooc-ungs/media-toolkit-pictures/pic2.jpg" class="titolo_gc_europe" download="Indonesia - Central JavaJuvenile prison Kutoarjo.jpg" target="_blank">Download Pic</a>
+                       <a href="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic02.jpg" class="titolo_gc_europe" download="MOOC UNGS-RECS - Pic02.jpg" target="_blank">Download Pic</a>
 
                     </div>
                 </div><!-- Close POST #2 -->
@@ -49,12 +59,12 @@
                 <!-- POST #3 -->
                 <div class="riga">
 					 <div class="tweet2">
-	                     <a href="files/eiuc/mooc-ungs/media-toolkit-pictures/pic3.jpg" class="titolo_gc_europe" download="Greece - Mersinidi Migration Detention Center Chios.jpg" target="_blank">
-		                   <img src="files/eiuc/mooc-ungs/media-toolkit-pictures/pic3.jpg" style="width:100%;" alt="placeholder">
+	                     <a href="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic03.jpg" class="titolo_gc_europe" download="MOOC UNGS-RECS - Pic03.jpg" target="_blank">
+		                   <img src="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic03.jpg" style="width:100%;" alt="placeholder">
 		                </a>
-		                <div class="pic-copyright">Greece - Migration Detention Centre</div>
+		                <div class="pic-copyright"></div>
                
-                       <a href="files/eiuc/mooc-ungs/media-toolkit-pictures/pic3.jpg" class="titolo_gc_europe" download="Greece - Mersinidi Migration Detention Center Chios.jpg" target="_blank">Download Pic</a>
+                       <a href="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic03.jpg" class="titolo_gc_europe" download="MOOC UNGS-RECS - Pic03.jpg" target="_blank">Download Pic</a>
 
                     </div>
                 </div><!-- Close POST #3 -->
@@ -62,12 +72,12 @@
                 <!-- POST #4 -->
                 <div class="riga">
 					 <div class="tweet2">
-	                     <a href="files/eiuc/mooc-ungs/media-toolkit-pictures/pic4.jpg" class="titolo_gc_europe" download="Jamaica - Fort Augusta Adult and Juvenile Correctional Centre.jpg" target="_blank">
-		                   <img src="files/eiuc/mooc-ungs/media-toolkit-pictures/pic4_small.jpg" style="width:100%;" alt="placeholder">
+	                     <a href="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic04.jpg" class="titolo_gc_europe" download="MOOC UNGS-RECS - Pic04.jpg" target="_blank">
+		                   <img src="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic04.jpg" style="width:100%;" alt="placeholder">
 		                </a>
-		                <div class="pic-copyright">Jamaica - Correctional Centre</div>
+		                <div class="pic-copyright"></div>
                
-                       <a href="files/eiuc/mooc-ungs/media-toolkit-pictures/pic4.jpg" class="titolo_gc_europe" download="Jamaica - Fort Augusta Adult and Juvenile Correctional Centre.jpg" target="_blank">Download Pic</a>
+                       <a href="files/eiuc/ungs-recs/Media-Toolkit/pics/MOOC%20UNGS-RECS%20-%20Pic04.jpg" class="titolo_gc_europe" download="MOOC UNGS-RECS - Pic04.jpg" target="_blank">Download Pic</a>
 
                     </div>
                 </div><!-- Close POST #4 -->
