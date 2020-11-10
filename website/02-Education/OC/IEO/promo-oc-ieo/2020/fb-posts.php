3e798205d47c26d035b28a48331f1b5be863df01
Bicet <bicets@gmail.com>
2020-11-10 14:04:39 +0100

    OC IEO: Media Toolkit - Initial Commit main files

---
+++ website/02-Education/OC/IEO/promo-oc-ieo/2020/fb-posts.php
@@ -0,0 +1,127 @@
+<div class="table_accordion_cell">
+        <div class="accordion accordion_gc">
+            <div class="icon_e_title_accordion">
+                <p class="title_accordion">Facebook</p>
+
+                <p class="number_universities">[nbsp]</p>
+
+                <div class="piu_accordion">
+                    <div class="verticale">
+                        [nbsp]
+                    </div>
+
+                    <div class="orizzontale">
+                        [nbsp]
+                    </div>
+                </div>
+            </div>
+
+            <div class="list_accordion">
+                <!--<p class="subtitle_page_programme titolo_gc_europe">Twitter</p>-->
+                
+                
+                
+			
+              	<!-- POSTS #1-2 -->
+                <div class="riga">
+                    <div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=1qwyEmbqebTxuqrA5lgtDvJi5daHQdYnh" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1qwyEmbqebTxuqrA5lgtDvJi5daHQdYnh" width="100%" alt="placeholder">
+		                </a>
+
+	                    <br><br>
+                        <p id="fp3">Join us this November and become aware of how democratic processes can make a difference for stability and peace around the world! #IEOdays<br><br>Registration deadline: 21 October 2019<br><br>Enrol by 12 September to receive a 10% discount<br><br>http://gchumanrights.org/ieo<br><br>@gchumanrights
+	                    </p><br><br><br>
+                        <a onclick="copyToClipboard('#fp3')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=1qwyEmbqebTxuqrA5lgtDvJi5daHQdYnh" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+                    </div>
+
+                    <div class="tweet2">
+	                <a href="https://drive.google.com/uc?export=view&id=17aYAU2Z5JeT0QRe9Xd0iQdpp7zAaKFBV" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=17aYAU2Z5JeT0QRe9Xd0iQdpp7zAaKFBV" width="100%" alt="placeholder">
+		                </a>
+					   <br><br>
+                        <p id="fp4">Since 2012, we have trained more than 400 future international electoral observers.<br><br>Discover more: http://gchumanrights.org/ieo<br><br>Deadline: 21 October 2019<br><br>Enrol by 12 September to receive a 10% discount!<br><br>The training is organised with the patronage of the Ministries of Foreign Affairs of Italy, Czech Republic and Spain.<br>@gchumanrights                 
+	                     </p><br>
+                         <a onclick="copyToClipboard('#fp4')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=17aYAU2Z5JeT0QRe9Xd0iQdpp7zAaKFBV" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #1 -->
+				<!-- POST #1 -->
+                <div class="riga">
+                    <div class="tweet2">
+	                    <a href="https://drive.google.com/uc?export=view&id=155XaIJQAAtjtdTyqq4Wgvuj8W_E4z4Rz" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=155XaIJQAAtjtdTyqq4Wgvuj8W_E4z4Rz" width="100%" alt="placeholder">
+		                </a>
+
+	                    <br><br>
+                        <p id="fp5">"The best parts of the International Electoral Observers Course were receiving hands-on training from experienced election observers and building relationships with the other participants. Until today, I am still in contact (and sometimes meet up on missions) with colleagues and friends I met during the course."<br><br>Deadline: 21 October 2019<br><br>Enrol by 12 September to receive a 10% discount!<br><br>http://gchumanrights.org/ieo<br>@gchumanrights  
+	                    </p><br>
+                        <a onclick="copyToClipboard('#fp5')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=155XaIJQAAtjtdTyqq4Wgvuj8W_E4z4Rz" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+                    </div>
+
+                    <div class="tweet2">
+	                <a href="https://drive.google.com/uc?export=view&id=1HIZSunpJhpozdBnzrjoi47HnOhOe3M2B" class="titolo_gc_europe" download target="_blank">
+		                    <img src="https://drive.google.com/uc?export=view&id=1HIZSunpJhpozdBnzrjoi47HnOhOe3M2B" width="100%" alt="placeholder">
+		                </a>
+					   <br><br>
+                        <p id="fp6">#IEOdays provide training to civilian staff in election observation missions at the first steps of their career.<br><br>Find out more: http://gchumanrights.org/ieo 🗳<br><br>Deadline: 21 October 2019<br><br>Enrol by 12 September to receive a 10% discount!<br>@gchumanrights                
+	                     </p><br><br><br><br><br><br>
+                         <a onclick="copyToClipboard('#fp6')" class="titolo_gc_europe" style="cursor: pointer;">Copy Caption</a> - <a href="https://drive.google.com/uc?export=view&id=1HIZSunpJhpozdBnzrjoi47HnOhOe3M2B" class="titolo_gc_europe" download target="_blank">Download Pic</a>
+
+                    </div>
+                </div><!-- Close POST #1 -->
+<!-- POST #1 -->
+                                
+                
+
+            </div>
+        </div>
+</div>
+
+<style type="text/css">
+.riga
+    {
+    display:flex;
+    flex-wrap: wrap;
+    border-spacing: 20px 10px; /*Optional*/
+
+    }
+ .tweet2
+    {
+
+    /*display: table-cell;*/
+    flex: 1 274px;
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: 274px;
+    padding: 20px;
+    margin: 10px;
+    border: 1px solid #e0dfdf;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+
+
+    }
+
+    </style>
+
+
+<script>
+function copyToClipboard(element) {
+  var $temp = $("<textarea>");
+  $("body").append($temp);
+  var html = $(element).html();
+  html = html.replace(/<br>/g, "\n"); // or \r\n
+  html = html.replace(/&amp;/, "&");
+
+  $temp.val(html).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+$( "#FailCopy" ).click(function() {
+  alert("Well done! div #error-details has been copy to your clipboard, now paste it in the notepad or email!");
+});
+</script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
